66e5e5cc64f6ad171e94f2d5a4399823df664b79
Alexei Vinidiktov <alexei@vinidiktov.ru>
2023-10-17 12:16:48 +0700

    Update dependency urls

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
     )
   ],
    dependencies: [
-    .package(url: "https://github.com/vinidiktov/BigIntKit.git", revision: "46ea3d4be50b198468f14f899753e58d85be7499"),
-    .package(url: "https://github.com/vinidiktov/OrderedDictionaryKit.git", revision: "e80bab83da7ac298c848bbec657c576a7260d8c0"),  
+    .package(url: "https://gitlab.e-imza.az/emilmsyv/BigIntKit.git", revision: "46ea3d4be50b198468f14f899753e58d85be7499"),
+    .package(url: "https://gitlab.e-imza.az/emilmsyv/OrderedDictionaryKit.git", revision: "e80bab83da7ac298c848bbec657c576a7260d8c0"),
    ],
   targets: [
     .target(
